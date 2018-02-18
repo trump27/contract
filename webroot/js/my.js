9fe5434c64e91c0d8241c4d077bc18e97669c42c
trump27 <smilebowl-trump27@yahoo.co.jp>
2018-02-18 12:26:00 +0900

    change datepicker's location

--- webroot/js/my.js
+++ webroot/js/my.js
@@ -1,7 +1,7 @@
 $(function () {
     
-    $('.datepicker').datepicker({
-        dateFormat: 'yy/mm/dd'
-    });
+    // $('.datepicker').datepicker({
+    //     dateFormat: 'yy/mm/dd'
+    // });
 
 });
